fa55fe2ec7353f11d0741301701b8d8b1f82e7cb
Rendi <rendicrew96@gmail.com>
2023-05-21 22:22:26 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='transition-colors bg-white dark:bg-dark dark:text-gray-300'>
         <Main />
         <NextScript />
       </body>
